29a222eb741179e23f588e1e9af6a1d518e1d9d7
BarreraJoel <joelmanuelbar@gmail.com>
2025-11-17 22:18:59 -0300

    fix: correccion rutas auth

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,7 +3,7 @@ import { generateToken } from "../utils/token-generator.js";
 export async function loginUser(email, password) {
     const userHard = { email: "test@mail.com", password: "123456" };
     if (userHard.email != email || userHard.password != password) {
-        throw new Error("Las credenciales no son correctas");
+        return false;
     }
     const userId = 1;
     const token = generateToken({ email: userHard.email, id: userId });
