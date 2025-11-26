6e0f705411e11be3a0697247c024ea82ba28e14e
BarreraJoel <joelmanuelbar@gmail.com>
2025-11-26 20:53:05 -0300

    fix: correccion credenciales

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import { generateToken } from "../utils/token-generator.js";
 
 export async function loginUser(email, password) {
-    const userHard = { email: "test@mail.com", password: "123456" };
+    const userHard = { email: "test@gmail.com", password: "123456" };
     if (userHard.email != email || userHard.password != password) {
         return false;
     }
